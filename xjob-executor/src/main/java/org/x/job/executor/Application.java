9e1c9ce2868abb21e86b6cf3bec69ff2df278850
EightMonth <eightmonth@qq.com>
2017-12-10 00:32:33 +0800

    Execute -> 增加zookeeper注册中心

--- xjob-executor/src/main/java/org/x/job/executor/Application.java
+++ xjob-executor/src/main/java/org/x/job/executor/Application.java
@@ -3,8 +3,10 @@ package org.x.job.executor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
